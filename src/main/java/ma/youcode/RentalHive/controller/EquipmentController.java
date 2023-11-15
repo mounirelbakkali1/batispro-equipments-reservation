5d90326045f323423e205ad46583696a745a7cff
amina <amina.elkabli1@gmail.com>
2023-11-15 15:12:37 +0100

    search

--- src/main/java/ma/youcode/RentalHive/controller/EquipmentController.java
+++ src/main/java/ma/youcode/RentalHive/controller/EquipmentController.java
@@ -8,14 +8,12 @@ import ma.youcode.RentalHive.dto.EquipmentResponseDTO;
 import ma.youcode.RentalHive.service.IEquipmentService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
-@RequestMapping("/api/v1/equipments")
+@RequestMapping("/v1/equipments")
 @RequiredArgsConstructor
 @Slf4j
 @RestController
@@ -23,9 +21,22 @@ public class EquipmentController {
 
     private final IEquipmentService  equipmentService;
 
+
+    @GetMapping({"/",""})
+    public ResponseEntity<List<EquipmentResponseDTO>> getAllEquipment(){
+        log.info("Request received to retrieve all equipments");
+        return ResponseEntity.ok(equipmentService.getAllEquipments());
+    }
+
+
     @PostMapping("/create")
     public ResponseEntity<EquipmentResponseDTO> createEquipment(@Valid @RequestBody EquipmentCreationRequestDTO requestDTO){
         log.info("Request received to create a new equipment.");
         return ResponseEntity.status(HttpStatus.CREATED).body(equipmentService.createEquipment(requestDTO));
+    }
+    @GetMapping("/{equipmentId}")
+    public ResponseEntity<EquipmentResponseDTO> getEquipmentById(@PathVariable Long equipmentId){
+        log.info("Request received to get equipment by ID: {}", equipmentId);
+        return ResponseEntity.ok(equipmentService.getEquipmentById(equipmentId));
     }
 }
--- src/main/java/ma/youcode/RentalHive/service/IEquipmentService.java
+++ src/main/java/ma/youcode/RentalHive/service/IEquipmentService.java
@@ -3,8 +3,11 @@ package ma.youcode.RentalHive.service;
 import ma.youcode.RentalHive.dto.EquipmentCreationRequestDTO;
 import ma.youcode.RentalHive.dto.EquipmentResponseDTO;
 
+import java.util.List;
+
 public interface IEquipmentService {
 
     EquipmentResponseDTO createEquipment (EquipmentCreationRequestDTO equipment);
-
+    EquipmentResponseDTO getEquipmentById(Long id);
+    List<EquipmentResponseDTO> getAllEquipments();
 }
