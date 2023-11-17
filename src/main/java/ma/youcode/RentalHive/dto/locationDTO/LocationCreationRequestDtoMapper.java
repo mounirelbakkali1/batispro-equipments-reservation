7a33ceaf9dc5cfb175a782214eb579042cd1bb4a
Mounir El Bakkali <mounir.elbakkali1@etu.uae.ac.ma>
2023-11-17 12:33:18 +0100

    new: afficher dossier de reservation avec statut

--- src/main/java/ma/youcode/RentalHive/dto/locationDTO/LocationCreationRequestDtoMapper.java
+++ src/main/java/ma/youcode/RentalHive/dto/locationDTO/LocationCreationRequestDtoMapper.java
@@ -1,2 +1,19 @@
-package ma.youcode.RentalHive.dto.locationDTO;public class LocationCreationRequestDtoMapper {
+package ma.youcode.RentalHive.dto.locationDTO;
+
+import ma.youcode.RentalHive.domain.entity.Location;
+import org.springframework.stereotype.Component;
+
+@Component
+public class LocationCreationRequestDtoMapper {
+
+
+    public LocationRequestDto mapToDto(Location location){
+        return LocationRequestDto.builder()
+                .status(location.getStatus().toString())
+                .startDate(location.getStartDate())
+                .endDate(location.getEndDate())
+                .quantity(location.getQuantity())
+                .equipmentReference(location.getEquipmentUnit().getEquipment().getModel())
+                .build();
+    }
 }
