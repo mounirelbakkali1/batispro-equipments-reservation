33ed20a14cb681373403b8e7f46f92fae0a00095
Mounir El Bakkali <mounir.elbakkali1@etu.uae.ac.ma>
2023-11-19 18:21:55 +0100

    added repo for eu

--- src/main/java/ma/youcode/RentalHive/repository/EquipmentUnitRepository.java
+++ src/main/java/ma/youcode/RentalHive/repository/EquipmentUnitRepository.java
@@ -1,2 +1,12 @@
-package ma.youcode.RentalHive.repository;public class EquipmentUnitRepository {
+package ma.youcode.RentalHive.repository;
+
+import ma.youcode.RentalHive.domain.entity.EquipmentUnit;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface EquipmentUnitRepository extends JpaRepository<EquipmentUnit, Long> {
+    Optional<EquipmentUnit> findByRef(String ref);
 }
