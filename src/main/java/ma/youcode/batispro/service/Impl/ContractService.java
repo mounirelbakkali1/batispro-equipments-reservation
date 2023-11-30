fedbd6c58b39068156c62d47a6b4d86578de694f
Mounir El Bakkali <mounir.elbakkali1@etu.uae.ac.ma>
2023-11-30 15:39:49 +0100

    update: validate the bill status before contract creation

--- src/main/java/ma/youcode/batispro/service/Impl/ContractService.java
+++ src/main/java/ma/youcode/batispro/service/Impl/ContractService.java
@@ -3,6 +3,8 @@ package ma.youcode.batispro.service.Impl;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import ma.youcode.batispro.domain.entity.Bill;
+import ma.youcode.batispro.domain.enums.BillStatus;
+import ma.youcode.batispro.exception.ContractCreationException;
 import ma.youcode.batispro.service.IBillingService;
 import ma.youcode.batispro.service.IContractGenerator;
 import ma.youcode.batispro.service.IContractService;
@@ -21,6 +23,9 @@ public class ContractService implements IContractService {
     @Override
     public byte[] createContract(String billNumber) throws Exception {
         Bill bill = billingService.getBillByNumber(billNumber);
+        if(!bill.getStatus().equals(BillStatus.ACCEPTED)) {
+            throw new ContractCreationException("Cannot generate contract for bill with status: " + bill.getStatus());
+        }
         log.info("Generating contract for bill: {}", billNumber);
         return contractGenerator.generateContract(bill);
     }
